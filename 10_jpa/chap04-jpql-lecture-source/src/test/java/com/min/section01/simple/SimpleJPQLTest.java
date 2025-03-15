0480f153cb5b20d4fbd6f0290835831a5b318970
mmmv41 <tlsalsrud5068@daum.net>
2025-03-15 10:01:39 +0900

    chap04 jpql 공부중

--- 10_jpa/chap04-jpql-lecture-source/src/test/java/com/min/section01/simple/SimpleJPQLTest.java
+++ 10_jpa/chap04-jpql-lecture-source/src/test/java/com/min/section01/simple/SimpleJPQLTest.java
@@ -1,4 +1,35 @@
 package com.min.section01.simple;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 public class SimpleJPQLTest {
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
 }
